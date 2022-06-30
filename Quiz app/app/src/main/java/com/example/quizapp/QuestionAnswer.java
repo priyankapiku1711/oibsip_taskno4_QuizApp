240555751fbdcc82962f1920b7fe4167b1cb78ad
priyankapiku1711 <priyankapiku1711@gmail.com>
2022-06-30 22:33:30 +0530

    My first File

---
+++ Quiz app/app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "World Environment Day is observed every year on?",
+            "Name the tallest building in the world.",
+            "Which is the fastest bird in the world?",
+            "Which is the world’s tallest monument?",
+            "Which company owns the android?"
+
+    };
+    public static String choices[][]={
+            {"13th january","26th August","30th September","5th june"},
+            {"Burj Khalifa, UAE","Shanghai Tower","Qutub Minar","Leaning Tower of Pisa"},
+            {"Ostrich", "Humming Bird" , "Peregrine Falcon", "Golden Eagle"},
+            {"Gateway Arch" , "Qutub Minar","Eiffel Tower ","The Empire State Building "},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+    public static String  CorrectAnswers[]={
+            "5th june",
+            "Burj Khalifa, UAE",
+            "Peregrine Falcon",
+            "Gateway Arch",
+            "Google"
+
+    };
+}
